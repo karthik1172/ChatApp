990e36bfb93e872a3ed36902b69c42a7cb4f1fa8
karthik1172 <karthik.p.rashinkar@gmail.com>
2025-11-28 18:53:59 +0530

    Initial set up

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Karthik Rashinkar on 28/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
